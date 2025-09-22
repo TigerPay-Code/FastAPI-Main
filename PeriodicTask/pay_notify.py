8676506e5e03af829bd57918a59922f918190f7e
18307 <183072530@qq.com>
2025-09-22 15:10:10 +0800

    加入定时任务

--- PeriodicTask/pay_notify.py
+++ PeriodicTask/pay_notify.py
@@ -28,13 +28,14 @@ def start_check_balance_task():
         func=start_check_balance,
         trigger=IntervalTrigger(minutes=1),
         id='minute_check_balance',
-        replace_existing=True
+        replace_existing=True,
+        start_date='2025-01-01 00:00:00',
+        end_date='2025-12-31 23:59:59'
     )
     scheduler.start()
     logger.info("一分钟定时任务已启动")
 
 def start_task():
     # 使用守护线程启动定时任务
-    thread = threading.Thread(target=start_check_balance_task, daemon=True)
-    thread.start()
+    threading.Thread(target=start_check_balance_task, daemon=True).start()
     logger.info("定时任务线程已启动")
