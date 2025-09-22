a2b68ded424ab43b73f3a6a4f04b047ec2500bf5
18307 <183072530@qq.com>
2025-09-22 13:40:23 +0800

    解决同时启动FastAPI和Telegram Bot的问题

--- Data/base.py
+++ Data/base.py
@@ -25,7 +25,7 @@ class Pay_RX_Notify_In_Data(BaseModel):
 
 
 class Pay_RX_Notify_Out_Data(BaseModel):
-    state: int = Field(title="支付状态", description="0-订单生成,1-支付中,2-支付成功,3-支付失败", default=0)
+    state: int = Field(title="支付状态", description="0-订单生成,1-付款中,2-付款成功,3-付款失败", default=0)
     sysOrderNo: str = Field(title="平台订单号", description="系统订单号", min_length=4, max_length=36)
     mchOrderNo: str = Field(title="下游订单号", description="商户订单号", min_length=4, max_length=36)
     amount: int = Field(title="金额", description="单位分", ge=1, le=1000000)
@@ -38,14 +38,3 @@ class Pay_RX_Notify_Refund_Data(BaseModel):
     mchOrderNo: str = Field(title="下游订单号", description="商户订单号", min_length=4, max_length=36)
     amount: int = Field(title="金额", description="单位分", ge=1, le=1000000)
     sign: str = Field(title="签名", description="签名值大写的MD5值", min_length=32, max_length=32)
-
-# try:
-#     data = Pay_RX_Notify_In_Data(
-#         state=3,  # 无效值
-#         sysOrderNo="ORD123456",
-#         mchOrderNo="MCH987654",
-#         amount=5000,
-#         sign="F0A7708FEB4B6C9C197A37AAA22FE3F4"
-#     )
-# except ValidationError as e:
-#     print(e.errors())
