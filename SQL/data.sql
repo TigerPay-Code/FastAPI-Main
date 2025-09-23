591a0b929c12d95c207ac6802c6e7cc05e0924f8
18307 <183072530@qq.com>
2025-09-23 12:17:34 +0800

    加入定时任务

--- SQL/data.sql
+++ SQL/data.sql
@@ -10,6 +10,7 @@ CREATE TABLE IF NOT EXISTS `telegram_users` (
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '主键ID',
     `username` VARCHAR(50) NOT NULL COMMENT '用户名',
     `is_admin` TINYINT(1) NOT NULL DEFAULT 0 COMMENT '管理员标识：0-普通用户, 1-管理员',
+    `nature` TINYINT(1) NOT NULL DEFAULT 1 COMMENT '用户性质：1-普通用户, 2-机器人, 3-群组',
     `status` TINYINT(1) NOT NULL DEFAULT 1 COMMENT '状态：0-失效, 1-正常',
     `chat_id` BIGINT NOT NULL COMMENT 'Telegram聊天ID',
     UNIQUE KEY `uniq_username` (`username`),
@@ -18,7 +19,7 @@ CREATE TABLE IF NOT EXISTS `telegram_users` (
 
 /* 插入初始数据 */
 /* 管理员用户 */
-INSERT INTO `telegram_users` (`username`, `is_admin`, `status`, `chat_id`) VALUES ('admin', 1, 1, 5312177749);
+INSERT INTO `telegram_users` (`username`, `is_admin`, `nature`, `status`, `chat_id`) VALUES ('admin', 1, 1, 1, 5312177749);
 
 /* 测试用户 */
-INSERT INTO `telegram_users` (`username`, `is_admin`, `status`, `chat_id`) VALUES ('测试', 0, 1, -4944286056);
+INSERT INTO `telegram_users` (`username`, `is_admin`, `nature`, `status`, `chat_id`) VALUES ('测试', 0, 3, 1, -4944286056);
