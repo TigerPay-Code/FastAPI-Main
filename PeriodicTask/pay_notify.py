0ceaeba7e211d3d57f481d16d34cad17d209ffc9
18307 <183072530@qq.com>
2025-09-26 13:23:51 +0800

    20250926

--- PeriodicTask/pay_notify.py
+++ PeriodicTask/pay_notify.py
@@ -49,13 +49,17 @@ def start_check_balance_task():
     #     end_date='2025-12-31 23:59:59'
     # )
 
+
+    time_gap = f"*/{public_config.get(key='task.interval', get_type=int, default=60)}"
+    print(time_gap)
+
     # 只在每天的上午9点到20点之间，每30分钟执行一次任务
     scheduler.add_job(
         func=run_async_task,
         trigger='cron',
         hour='9-20',  # 只在上午9点到下午8点（20点）之间运行
         # minute='*/30',  # 每隔30分钟运行一次
-        second=f'*/{public_config.get(key="task.interval", get_type=int, default=60)}',  # 每隔多少秒钟运行一次
+        second=time_gap,  # 每隔多少秒钟运行一次
         start_date='2025-01-01 00:00:00',  # 任务开始时间 2025-01-01 00:00:00
         end_date='2025-12-31 23:59:59'  # 任务结束时间 2025-12-31 23:59:59
     )
