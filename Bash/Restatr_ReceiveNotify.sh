6da7e7bdd862874f03974ef2aae4211d9abc01f5
18307 <183072530@qq.com>
2025-09-28 11:27:01 +0800

    20250928

--- Bash/Restatr_ReceiveNotify.sh
+++ Bash/Restatr_ReceiveNotify.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+echo "正在更新代码..."
 cd /data/FastAPI-Main
+echo "停止服务..."
 sudo systemctl stop receive-notify.service
+echo "清理日志..."
 sudo rm -rf /data/FastAPI-Main/logs/*.log
+echo "更新代码..."
 git pull
+sleep 3
+echo "赋予脚本执行权限..."
 chmod +x /data/FastAPI-Main/Bash/*.sh
+echo "重启服务..."
 sudo systemctl restart receive-notify.service
 sleep 3
 clear
+echo "查看日志..."
 sudo tail -f /data/FastAPI-Main/logs/ReceiveNotify.log
